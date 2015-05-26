7c15b27a444369bd7c4ec429480276cc478b1671
顏孜羲 <joseph.yen@gmail.com>
2015-05-27 04:17:05 +0800

    combiner

--- ngramcount.py
+++ ngramcount.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
-
+import fileinput
+from itertools import groupby, imap
 
 def ngrams(words):
     for length in range(1, 5 + 1):
@@ -18,32 +19,39 @@ def mapper(line):
         yield ' '.join(ngram), 1
 
 
+ 
+        
 def reducer(key, values):
     count = sum(int(v) for v in values)
     yield key, count
-
+    
+combiner = reducer
 
 def do_mapper(files):
-    import fileinput
     for line in fileinput.input(files):
         for key, value in mapper(line):
-            print('{}\t{}'.format(key, value))
+            yield key, value
 
-
-def line_to_keyvalue(line):
-    key, value = line.decode('utf8').split('\t', 1)
-    return key, value
-
+def do_combiner(mapper_out):
+    mapper_out = sorted(mapper_out, key = lambda x: x[0])
+    for key, grouped_keyvalues in groupby(mapper_out,
+                                          key = lambda x: x[0]):
+        values = (v for k, v in grouped_keyvalues)
+        for key, value in combiner(key, values):
+            yield key, value
 
 def do_reducer(files):
-    import fileinput
-    from itertools import groupby, imap
+    def line_to_keyvalue(line):
+        key, value = line.decode('utf8').split('\t', 1)
+        return key, value
+
     keyvalues = imap(line_to_keyvalue, fileinput.input(files))
     for key, grouped_keyvalues in groupby(keyvalues,
                                           key=lambda x: x[0]):
         values = (v for k, v in grouped_keyvalues)
         for key, value in reducer(key, values):
-            print('{}\t{}'.format(key, value))
+            yield key, value
+            
 
 
 def argparser():
@@ -62,6 +70,12 @@ if __name__ == '__main__':
 
     args = argparser()
     if args.mapper:
-        do_mapper(args.files)
+        mapper_out = do_mapper(args.files)
+        # mapper_out = do_combiner(mapper_out)
+        for key, value in mapper_out:
+            print('{}\t{}'.format(key, value))
+            
     elif args.reducer:
-        do_reducer(args.files)
+        reducer_out  = do_reducer(args.files)
+        for key, value in reducer_out:
+            print('{}\t{}'.format(key, value))
